a546b712e54257e4a92d3b1c3a2f231d841b0df1
hg.park <hangil6061@naver.com>
2019-05-14 16:30:49 +0900

    멀티스타일 텍스트 데이터 추가

--- initFile/unity/Assets/01_Scripts/component/MultiStyleText.cs
+++ initFile/unity/Assets/01_Scripts/component/MultiStyleText.cs
@@ -1,7 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Style
+{
+    public string name;
+    public Font font;
+    public int fontSize;
+    public TextAnchor alignment;
+    public Color color;
+}
 
 public class MultiStyleText : MonoBehaviour {
-
+    public Style[] styles;
 }
--- initFile/unity/Assets/01_Scripts/component/MultiStyleTextData.cs
+++ initFile/unity/Assets/01_Scripts/component/MultiStyleTextData.cs
@@ -3,9 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class StyleData
+{
+    public string name;
+    public string font;
+    public int fontSize;
+    public string alignment;
+    public string color;
+
+    public StyleData( Style style )
+    {
+        name = style.name;
+        font = style.font.name;
+        fontSize = style.fontSize;
+        alignment = style.alignment.ToString();
+        color = Util.ColorToHex(style.color);
+    }
+}
+
+[System.Serializable]
 public class MultiStyleTextData : TextData {
+
+    public StyleData[] styles;
+
     public MultiStyleTextData(Text text) : base(text)
     {
         name = "multiStyleText";
+
+        MultiStyleText multiStyle = text.GetComponent<MultiStyleText>();
+
+        if( multiStyle.styles.Length > 0 )
+        {
+            styles = new StyleData[multiStyle.styles.Length];
+            for( int i = 0; i < styles.Length; i++ )
+            {
+                styles[i] = new StyleData(multiStyle.styles[i]);
+            }
+        }
     }
 }
